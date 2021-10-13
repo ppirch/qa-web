caed93ae79386a2766a89c2be8663c49350dc3a6
Pakin Pirch <pakin.pirch@gmail.com>
2021-10-13 16:14:27 +0000

    Add QA-Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {
   Container, Typography, Box, Link, AppBar, TextField, Button, Toolbar, Stack,
   Select,
-  SelectChangeEvent, InputLabel, FormControl, MenuItem, Card, CardActions, CardContent, CardMedia, CircularProgress
+  SelectChangeEvent, InputLabel, FormControl, MenuItem, Card, CardActions, CardContent, CardMedia, CircularProgress,
+  Tab, Tabs
 } from '@mui/material'
 import { styled, alpha } from '@mui/material/styles';
 import axios from 'axios'
@@ -41,12 +42,61 @@ function Copyright() {
   );
 }
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 2 }}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 function App() {
 
   const [question, setQuestion] = useState<string>('Machine Learning คืออะไร');
-  const [model, setModel] = useState<string>('wangchanberta');
+  const [questionContext, setQuestionContext] = useState<string>('เกิดวันที่เท่าไร');
+  const [context, setContext] = useState<string>(`พัทธ์ธีรา ศรุติพงศ์โภคิน (เกิด 3 ธันวาคม พ.ศ. 2533) หรือชื่อเล่นว่า อร เป็นนักแสดงหญิงชาวไทย สำเร็จมัธยมศึกษาจากCatholic Cathedral College ประเทศนิวซีแลนด์ และปริญญาตรีจากRaffles International College สาขา Business Marketing เข้าสู่วงการตั้งแต่อายุ 6 ขวบ จากการแสดงละครเวทีกับ ครูชลประคัลภ์ จันทร์เรือง จากนั้นก็เล่นโฆษณาในวัยเด็ก 2- 3 ชิ้น และยังเคยแสดงช่วงละครสั้น ในรายการซุปเปอร์จิ๋ว ประมาณปี 2542 ปัจจุบันเป็นทั้ง นักแสดง , พิธีกร และ วีเจ อยู่ที่คลื่น เก็ท 102.5 Bangkok International Hits Music Station และยังเป็นพิธีกรให้กับช่อง ทรู มิวสิก`);
+  const [model, setModel] = useState<string>('xlm-roberta');
   const [answers, setAnswers] = useState<Array<AnswerObj>>([]);
+  const [answerContext, setAnswerContext] = useState<Answer>();
   const [loading, setLoading] = useState<boolean>(false);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleQuestionContextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestionContext(event.target.value);
+  };
+
+  const handleContextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContext(event.target.value);
+  };
 
   const handleQuestionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuestion(event.target.value);
@@ -54,6 +104,15 @@ function App() {
 
   const handleModelChange = (event: SelectChangeEvent) => {
     setModel(event.target.value as string);
+  };
+
+  const handleGo2Click = async () => {
+    const API_URL = "https://pirch-api.everythink.dev/qa-context/"
+    setLoading(true)
+    setAnswerContext(undefined)
+    const res = await axios.post<Answer>(API_URL, { context: context, question: questionContext, model: model })
+    setLoading(false)
+    setAnswerContext(res.data)
   };
 
   const handleGoClick = async () => {
@@ -87,59 +146,121 @@ function App() {
             flexDirection: 'column',
             minHeight: '80vh',
           }}>
-          <Stack spacing={2} direction="row">
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="QA" {...a11yProps(0)} />
+            <Tab label="QA-Context" {...a11yProps(1)} />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            <Stack spacing={2} direction="row">
+              <TextField
+                required
+                id="questin-input"
+                label="Question"
+                value={question}
+                onChange={handleQuestionChange}
+                fullWidth
+              />
+              <FormControl sx={{ m: 1, minWidth: "15%" }} >
+                <InputLabel id="qa-model">Model</InputLabel>
+                <Select
+                  labelId="qa-model"
+                  id="qa-model-select"
+                  value={model}
+                  label="Model"
+                  onChange={handleModelChange}
+                >
+                  <MenuItem value={"xlm-roberta"}>XLM-RoBERTa</MenuItem>
+                  <MenuItem value={"wangchanberta"}>WangchanBERTa</MenuItem>
+                  <MenuItem value={"bert"}>BERT</MenuItem>
+                </Select>
+              </FormControl>
+              <Button variant="outlined" onClick={handleGoClick} disabled={loading}>GO</Button>
+            </Stack>
+            {loading && <Box sx={{ display: 'flex' }} alignItems="center" justifyContent="center" marginTop={4}> <CircularProgress /> </Box>}
+            <Stack sx={{
+              mt: 2,
+            }} spacing={2}>
+              {answers.map((answer, index) =>
+              (
+                <Card variant="outlined" key={index}>
+                  <CardContent>
+                    <Typography sx={{ mt: 1.5 }} color="text.secondary">
+                      {answer.context.title}
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                      {answer.answer.answer.replaceAll(".", "")}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      Score : {answer.answer.score.toFixed(5)}
+                    </Typography>
+                    <Typography variant="body2">
+                      {answer.context.text}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" href={answer.context.link + "#:~:text=" + answer.answer.answer.replaceAll(".", "")} target="_blank">Learn More</Button>
+                  </CardActions>
+                </Card>
+              )
+              )}
+            </Stack>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
             <TextField
-              required
-              id="questin-input"
-              label="Question"
-              value={question}
-              onChange={handleQuestionChange}
               fullWidth
+              id="outlined-multiline-static"
+              label="Context"
+              multiline
+              rows={14}
+              value={context}
+              onChange={handleContextChange}
             />
-            <FormControl sx={{ m: 1, minWidth: "15%" }} >
-              <InputLabel id="qa-model">Model</InputLabel>
-              <Select
-                labelId="qa-model"
-                id="qa-model-select"
-                value={model}
-                label="Model"
-                onChange={handleModelChange}
-              >
-                <MenuItem value={"wangchanberta"}>WangchanBERTa</MenuItem>
-                <MenuItem value={"xlm-roberta"}>XLM-RoBERTa</MenuItem>
-                <MenuItem value={"bert"}>BERT</MenuItem>
-              </Select>
-            </FormControl>
-            <Button variant="outlined" onClick={handleGoClick} disabled={loading}>GO</Button>
-          </Stack>
-          {loading && <Box sx={{ display: 'flex'}} alignItems="center" justifyContent="center" marginTop={4}> <CircularProgress /> </Box>}
-          <Stack sx={{
-            mt: 2,
-          }} spacing={2}>
-            {answers.map((answer, index) =>
-            (
-              <Card variant="outlined" key={index}>
+            <Stack sx={{ mt: 2 }} spacing={2} direction="row">
+              <TextField
+                required
+                id="questin-input"
+                label="Question"
+                value={questionContext}
+                onChange={handleQuestionContextChange}
+                fullWidth
+              />
+              <FormControl sx={{ mt: 1, minWidth: "15%" }} >
+                <InputLabel id="qa-model">Model</InputLabel>
+                <Select
+                  labelId="qa-model"
+                  id="qa-model-select"
+                  value={model}
+                  label="Model"
+                  onChange={handleModelChange}
+                >
+                  <MenuItem value={"xlm-roberta"}>XLM-RoBERTa</MenuItem>
+                  <MenuItem value={"wangchanberta"}>WangchanBERTa</MenuItem>
+                  <MenuItem value={"bert"}>BERT</MenuItem>
+                </Select>
+              </FormControl>
+              <Button variant="outlined" onClick={handleGo2Click} disabled={loading}>GO</Button>
+            </Stack>
+            {loading && <Box sx={{ display: 'flex' }} alignItems="center" justifyContent="center" marginTop={4}> <CircularProgress /> </Box>}
+            {
+              answerContext &&
+              <Card variant="outlined" sx={{ marginTop: 2 }}>
                 <CardContent>
-                  <Typography sx={{ mt: 1.5 }} color="text.secondary">
-                    {answer.context.title}
-                  </Typography>
                   <Typography variant="h5" component="div">
-                    {answer.answer.answer.replaceAll(".", "")}
+                    Answer :  {answerContext.answer}
                   </Typography>
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    Score : {answer.answer.score.toFixed(5)}
+                  <Typography >
+                    Score : {answerContext.score.toFixed(5)}
                   </Typography>
-                  <Typography variant="body2">
-                    {answer.context.text}
+                  <Typography>
+                    Start : {answerContext.start}
+                  </Typography>
+                  <Typography >
+                    End : {answerContext.end}
                   </Typography>
                 </CardContent>
-                <CardActions>
-                  <Button size="small" href={answer.context.link + "#:~:text=" + answer.answer.answer.replaceAll(".", "")} target="_blank">Learn More</Button>
-                </CardActions>
               </Card>
-            )
-            )}
-          </Stack>
+            }
+          </TabPanel>
         </Box>
       </Container>
       <Copyright />
